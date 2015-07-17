6a9cf887ce12cc9c29d3de904f57f9c5f4b2b3c8
William Claude Tumeo <tumeowilliam@gmail.com>
2015-07-17 15:49:17 -0300

    Mais coisasinhas

--- index.php
+++ index.php
@@ -20,17 +20,16 @@ if(!$link){
     echo "erro ao conectar ao banco de dados!";exit();
 }
 
-$SQL = "SELECT * FROM crest";
-$RS  = mysql_query("SELECT * FROM CREST");
-$selecMyth = mysql_query("SELECT * FROM MEMBER WHERE FACE_ID = 1489576273");
-$arrayMyth = mysql_fetch_array($selecMyth);
-while($RF = mysql_fetch_array($RS))
-{
 
-    print_r($RF['titulo']);
-
-}
-
+#1489576273
+$selectMyth = mysql_query("SELECT * FROM MEMBER");
+while ($myth = mysql_fetch_array($selectMyth)){
+    $crest_id = $myth["Crest_crest_id"];
+    $selectCrest = mysql_query("SELECT * FROM CREST WHERE CREST_ID = $crest_id");
+    $crest = mysql_fetch_array($selectCrest);
+    var_dump($crest);
+    
+    
  echo '<div id="tabelas">';
  echo '<table  class="tg" style="table-layout: margin-bottom: 10px; fixed; width: 100%">';
  echo '<colgroup>';
@@ -39,11 +38,11 @@ while($RF = mysql_fetch_array($RS))
  echo '<tr>';
           #<!-- CREST ICON -->
  echo '<th class="crest">';
- echo '<img class="crestpic" title="','CrestTitle','" src="',$PATH,'CrestIMG','.png">';
+ echo '<img class="crestpic" title="',$crest["titulo"],'" src="',$PATH,$crest["img"],'.png">';
  echo '</th>';
           #<!-- NAME -->
  echo '<th align="left" class="name" colspan="3">';
- echo $arrayMyth["usual_name"];
+ echo $myth["usual_name"];
  echo '</th>'; 
  #<!-- Espacinho -->
  #<!-- TIER -->
@@ -53,22 +52,20 @@ while($RF = mysql_fetch_array($RS))
  echo '<tr>';
           #<!-- PROFILE PIC -->
  echo '<td align="center" class="profilepic" colspan="2" rowspan="5">';
- echo '<a target="_blank" href="https://www.facebook.com/',$arrayMyth["face_id"],'">';
- echo '<img class="profile" src="http://graph.facebook.com/',$arrayMyth["face_id"],'/picture?width=151&height=151"></a>';
+ echo '<a target="_blank" href="https://www.facebook.com/',$myth["face_id"],'">';
+ echo '<img class="profile" src="http://graph.facebook.com/',$myth["face_id"],'/picture?width=151&height=151"></a>';
+
+ echo '</td><tr><td HEIGHT="1px" colspan="2"></td></tr>';
+ echo '<td class="tg-e3zv"> Badges</td>';
+ echo '<td class="tg-031e"></td>';
+ echo '<td class="since" align="right"  colspan="3" >Mitando since  <strong class="mythano">Myth Ano</strong></td>';
+ echo '</tr><tr>';
+          #<!-- BADGES -->
+ echo '<td class="badges" id="MythID" colspan="4">';
+ echo '</td></tr>';
+ echo '<tr><td class="badges"><td class="badges"><td colspan="2" align="right" class="badges2">  <i>Badges Coletadas:</i> <b id="MitoID"></b> </td></tr>';
+ echo '</table></div><br>';
+}
 ?>
-          </td><tr><td HEIGHT="1px" colspan="2" ></td></tr>
-          <td class="tg-e3zv"> Badges
-          </td>
-          <td class="tg-031e"></td>
-          <td class="since" align="right"  colspan="3" >Mitando since  <strong class="mythano">Myth Ano</strong></td>
-        </tr>
-        <tr>
-          <!-- BADGES -->
-          <td class="badges" id="MythID" colspan="4">
-          </td>
-        </tr>
-        <tr><td class="badges"><td class="badges"><td colspan="2" align="right" class="badges2">  <i>Badges Coletadas:</i> <b id="MitoID"></b> </td></tr>
-        </table>
-        </div><br>
     </body>
 </html>
